a172222a7a5430fed7033ac54650553ec3fb014e
Tony-Gmz <gta.ide@hotmail.com>
2020-05-25 16:41:00 +0200

    modal static for connexion and inscription

--- frontend/src/components/App/index.js
+++ frontend/src/components/App/index.js
@@ -1,14 +1,18 @@
 // == Import npm
 import React from 'react';
 
-// == Import
 
+import ModalInscription from 'src/components/ModalInscription';
+import ModalConnexion from 'src/components/ModalConnexion';
+
+// == Import
 import './styles.css';
 
 // == Composant
 const App = () => (
   <div className="app">
-  
+    <div><ModalConnexion /></div>
+    <div><ModalInscription /></div>
   </div>
 );
 
--- frontend/src/components/ModalConnexion/index.js
+++ frontend/src/components/ModalConnexion/index.js
@@ -1,5 +1,6 @@
 // == Import npm
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 // == Import
 import './modalConnexion.scss';
@@ -7,7 +8,41 @@ import './modalConnexion.scss';
 // == Composant
 const ModalConnexion = () => (
   <div className="ModalConnexion">
-   
+    <a href="#connexion" className="connexion_btn">Connexion</a>
+    <div id="connexion" className="connexion">
+      <div className="modal_block">
+        <div className="modal_header">
+          <p className="modal_title">Bienvenue dans notre espace Connexion</p>
+        </div>
+        <form>
+          <div className="form_element">
+            <TextField
+              className="input"
+              id="outlined-password-input"
+              label="Adresse Email"
+              type="email"
+              autoComplete="current-username"
+              variant="outlined"
+            />
+          </div>
+          <div className="form_element">
+            <TextField
+              className="input"
+              id="outlined-password-input"
+              label="Mot de passe"
+              type="password"
+              autoComplete="current-password"
+              variant="outlined"
+            />
+          </div>
+        </form>
+        <div className="div_submit">
+          <button className="submit_btn" type="submit">Connexion</button>
+          <button className="cancel_btn" type="submit">Annuler</button>
+        </div>
+        <a href="#" className="close_btn">close</a>
+      </div>
+    </div>
   </div>
 );
 
