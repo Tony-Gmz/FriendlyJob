446982bf5cb777a9915878292ef846a8839252df
poulpy75 <t.clusel@gmail.com>
2020-05-27 11:36:03 +0200

    we well receive the action GET_TOKEN in middleware

--- frontend/src/middlewares/servicesMiddleware.js
+++ frontend/src/middlewares/servicesMiddleware.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
 const servicesMiddleware = (store) => (next) => (action) => {
-
-  
+  console.log('on a intercepté une action dans le middleware: ', action);
+  switch (action.type) {
+    default:
+      // on passe l'action au suivant (middleware suivant ou reducer)
+      next(action);
+  }
 };
 
 export default servicesMiddleware;
--- frontend/src/middlewares/userMiddleware.js
+++ frontend/src/middlewares/userMiddleware.js
@@ -5,17 +5,10 @@ import { GET_TOKEN } from '../action/userActions';
 const userMiddleware = (store) => (next) => (action) => {
   console.log('on a intercepté une action dans le middleware: ', action);
   switch (action.type) {
-    case GET_TOKEN:
-      axios
-
-      next(action);
-      break;
     default:
       // on passe l'action au suivant (middleware suivant ou reducer)
       next(action);
   }
-
- 
 };
 
 export default userMiddleware;
