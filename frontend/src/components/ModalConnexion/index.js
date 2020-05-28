f9cc9d302f8aa2e230fce61c0a1246355734fc04
poulpy75 <t.clusel@gmail.com>
2020-05-28 11:34:16 +0200

    resolve bug

--- frontend/src/components/ModalConnexion/index.js
+++ frontend/src/components/ModalConnexion/index.js
@@ -19,48 +19,47 @@ const ModalConnexion = () => {
 
   return (
     <div className="ModalConnexion">
-    <a href="#connexion"><NavButtonStyled>Connexion</NavButtonStyled></a>
-    <div id="connexion" className="connexion">
-      <div className="modal_block">
-        <div className="modal_header">
-          <p className="modal_title">Bienvenue dans notre espace Connexion</p>
-        </div>
-        <form onSubmit={handleSubmit}>
-          <div className="form_element">
-            <TextField
-              className="input"
-              id="outlined-password-input"
-              label="Adresse Email"
-              type="email"
-              autoComplete="current-username"
-              variant="outlined"
-              name="email"
-              onChange={handleChange}
-            />
+      <a href="#connexion"><NavButtonStyled>Connexion</NavButtonStyled></a>
+      <div id="connexion" className="connexion">
+        <div className="modal_block">
+          <div className="modal_header">
+            <p className="modal_title">Bienvenue dans notre espace Connexion</p>
           </div>
-          <div className="form_element">
-            <TextField
-              className="input"
-              id="outlined-password-input"
-              label="Mot de passe"
-              type="password"
-              autoComplete="current-password"
-              variant="outlined"
-              name="password"
-              onChange={handleChange}
-            />
-          </div>
-        </form>
-        <div className="div_submit">
-          <button className="submit_btn" type="submit">Connexion</button>
-          <button className="cancel_btn" type="submit">Annuler</button>
+          <form onSubmit={handleSubmit}>
+            <div className="form_element">
+              <TextField
+                className="input"
+                id="outlined-password-input"
+                label="Adresse Email"
+                type="email"
+                autoComplete="current-username"
+                variant="outlined"
+                name="email"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="form_element">
+              <TextField
+                className="input"
+                id="outlined-password-input"
+                label="Mot de passe"
+                type="password"
+                autoComplete="current-password"
+                variant="outlined"
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+            <div className="div_submit">
+              <button className="submit_btn" type="submit">Connexion</button>
+              <button className="cancel_btn" type="submit">Annuler</button>
+            </div>
+          </form>
+          <a href="#" className="close_btn">close</a>
         </div>
-        <a href="#" className="close_btn">close</a>
       </div>
     </div>
-    </div>
-  )
-  
+  );
 };
 
 // == Export
