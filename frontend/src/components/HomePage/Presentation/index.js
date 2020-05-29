3fb5d4e1fa55bfa654a6baf525d5e72cabdecb7b
poulpy75 <t.clusel@gmail.com>
2020-05-29 15:06:12 +0200

    create dynamic Input with state Data for selectService

--- frontend/src/components/HomePage/Presentation/index.js
+++ frontend/src/components/HomePage/Presentation/index.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 import { Input } from 'semantic-ui-react';
 import './presentation.scss';
 import { location, useHistory } from 'react-router-dom';
+import { selectService } from '../../../action/servicesActions';
 
 // component presentation in homepage
 
-const Presentation = ({ serviceList }) => {
+const Presentation = ({ serviceList, fieldService, serviceSelected }) => {
   const history = useHistory();
   const handleSubmit = (evt) => {
     evt.preventDefault();
     console.log('coucou je suis un submit sur la homepage');
-    history.push("/services/jardinage");
+    history.push(`/services/${serviceSelected}`);
   };
   const handleChange = (evt) => {
     console.log(`value: ${evt.target.value}`);
+    fieldService(evt.target.value);
   };
   return (
     <div className="presentation">
@@ -36,6 +38,9 @@ const Presentation = ({ serviceList }) => {
 };
 
 Presentation.propTypes = {
+  serviceSelected: PropTypes.string.isRequired,
+  /** Func with param */
+  fieldService: PropTypes.func.isRequired,
   serviceList: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
