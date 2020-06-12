6b705aad30da92f324ac11fe96185fd174eab68d
poulpy75 <t.clusel@gmail.com>
2020-06-12 10:32:42 +0200

    solv bug

--- frontend/src/components/NaviviDad/Navivi/Brand/index.js
+++ frontend/src/components/NaviviDad/Navivi/Brand/index.js
@@ -5,9 +5,6 @@ import { Link } from 'react-router-dom';
 const Brand = () => {
   return (
     <>
-      <Link to="/">
-        <Image src="https://res.cloudinary.com/friendlyjob/image/upload/e_cartoonify,q_100/v1591887558/friendlyjob/logo_svg_lc4egu.png" />
-      </Link>
     </>
   );
 };
