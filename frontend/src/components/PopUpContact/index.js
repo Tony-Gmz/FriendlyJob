5a0d3bb50378ad696a246c0ebef47f371764c8be
poulpy75 <t.clusel@gmail.com>
2020-06-15 08:58:30 +0200

    add style on modal and button for contact

--- frontend/src/components/PopUpContact/index.js
+++ frontend/src/components/PopUpContact/index.js
@@ -11,13 +11,13 @@ const PopupContact = ({request, getCommentId }) => {
     <Popup
       header={`Voici les coordonnés de ${request.jobWorker.firstname}`}
       description={`@:${request.jobWorker.email}`}
-      content={<a href={`mailto:${request.jobWorker.email}`}>{request.jobWorker.email} </a>}
+      content={<> <i class="envelope icon" /> : <a href={`mailto:${request.jobWorker.email}`}>{request.jobWorker.email}</a> </>}
       on="click"
       position="top right"
       size="large"
       hideOnScroll
       pinned
-      trigger={<Button onClick={handleClick} content="Contacter" />}
+      trigger={<Button onClick={handleClick}><i class="comment alternate outline icon" /> Contacter</Button>}
     />
   );
 };
